9cc1c22863f9d8828f49be3d05ba87d709de52e5
lunyi476 <lunyi476@yahoo.com>
2023-09-17 17:37:11 -0400

    update changes

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,17 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
+//import path from 'path';
+// const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 module.exports = function (config) {
-  config.set({
+  config.set ({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-jasmine-html-reporter',
+      'karma-coverage-istanbul-reporter',
+      '@angular-devkit/build-angular/plugins/karma'
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -29,4 +30,4 @@ module.exports = function (config) {
     singleRun: false,
     restartOnFileChange: true
   });
-};
+}
